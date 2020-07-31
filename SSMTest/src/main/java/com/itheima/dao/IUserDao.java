da732690943c269033834992e770f319cc91d5a6
ruansixiaotianshi <1916708674@qq.com>
2020-07-31 22:21:05 +0800

    2020.07.31

---
+++ SSMTest/src/main/java/com/itheima/dao/IUserDao.java
@@ -0,0 +1,19 @@
+package com.itheima.dao;
+
+import com.itheima.domain.User;
+
+import java.util.List;
+
+/**
+ * 用户的持久层接口
+ */
+public class IUserDao {
+    /**
+     * 查询所有操作
+     *
+     * @return
+     */
+    List<User> findAll() {
+        return null;
+    }
+}
